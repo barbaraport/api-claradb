79ec3566e3ed43fa4d739a2a112199627aeda65e
Rafael Furtado <rafs.97@hotmail.com>
2022-04-02 08:45:22 -0300

    Ajustando interface inicial

--- src/client/ClaraClient/src/view/components/FolconnApp.tsx
+++ src/client/ClaraClient/src/view/components/FolconnApp.tsx
@@ -8,6 +8,15 @@ const styles = StyleSheet.create({
             fontWeight: 'bold',
             fontSize: 30,
             margin: 20
+        },
+        phoneToolbar: {
+            width: "100%",
+            height: 25,
+            backgroundColor: "#7dd6fb"
+        },
+        screen: {
+            width: "100%",
+            height: "100%",
         }
     }
 );
@@ -16,7 +25,8 @@ export class FolconnApp extends Component<any, any> {
     private buildComponent(){
         let component = (
             <SafeAreaView>
-                <View>
+                <View style={styles.screen}>
+                    <View style={styles.phoneToolbar}/>
                     <Text style={styles.bigBlue}>Texto inicial</Text>
                 </View>
             </SafeAreaView>
