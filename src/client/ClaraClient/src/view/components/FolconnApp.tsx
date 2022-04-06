c204536ce9ef54649453643a66f608806b50b4f1
Carolina M <carolinamargiott2i@gmail.com>
2022-04-06 12:14:54 -0300

    terminado função de goback, e ajustes na renderização da pagina

--- src/client/ClaraClient/src/view/components/FolconnApp.tsx
+++ src/client/ClaraClient/src/view/components/FolconnApp.tsx
@@ -1,56 +1,83 @@
 import { registerRootComponent } from "expo";
 import React, { Component } from "react";
-import { SafeAreaView, StatusBar, View } from "react-native";
+import { Button, SafeAreaView, StatusBar, View } from "react-native";
 import { PageAliases } from "../../enumerations/PageAliases";
 import { HomePage } from "../pages/HomePage";
 import { LoginPage } from "../pages/LoginPage";
 import { Styles } from "../styles/Styles";
 
 interface FolconnAppState {
-    currentPage: PageAliases;
+	currentPage: PageAliases;
+	pageHistory: Array<PageAliases>;
 }
 
 export class FolconnApp extends Component<any, FolconnAppState> {
-    constructor(props: any) {
-        super(props);
+	constructor(props: any) {
+		super(props);
 
-        this.state = {
-            currentPage: PageAliases.LOGIN
-        };
+		this.state = {
+			currentPage: PageAliases.LOGIN,
+			pageHistory: [PageAliases.LOGIN],
+		};
 
-        this.changeCurrentPage = this.changeCurrentPage.bind(this);
+		this.changeCurrentPage = this.changeCurrentPage.bind(this);
+		this.goBack = this.goBack.bind(this);
+		this.getPageToRender = this.getPageToRender.bind(this);
+	}
 
-    }
+	private changeCurrentPage(pageToChange: PageAliases) {
+		let pageHistoryMock: Array<PageAliases> = this.state["pageHistory"];
+		pageHistoryMock.push(pageToChange);
+		this.setState({
+			currentPage: pageToChange,
+			pageHistory: pageHistoryMock,
+		});
+	}
 
-    private changeCurrentPage(pageToChange: PageAliases){
-        this.setState({"currentPage": pageToChange});
+	private getPageToRender(): JSX.Element {
+		const loginPage: JSX.Element = (
+			<LoginPage pageRedirectFunction={this.changeCurrentPage} />
+		);
 
-    }
+		const homePage: JSX.Element = (
+			<HomePage pageRedirectFunction={this.changeCurrentPage} />
+		);
 
-    private buildComponent(){
-        let component = (
-            <SafeAreaView>
-                <StatusBar barStyle={"light-content"}/>
-                <View style={Styles.screen}>
-                    {this.state["currentPage"] === PageAliases.LOGIN &&
-                        <LoginPage pageRedirectFunction={this.changeCurrentPage}/>
+		switch (this.state["currentPage"]) {
+			case PageAliases.HOME:
+				return homePage;
 
-                    }
-                    {this.state["currentPage"] === PageAliases.HOME &&
-                        <HomePage pageRedirectFunction={this.changeCurrentPage}/>
-                    }
-                </View>
-            </SafeAreaView>
-        );
+			case PageAliases.LOGIN:
+				return loginPage;
 
-        return component;
-    }
+			default:
+				return homePage;
+		}
+	}
 
-    render() {
-        let component = this.buildComponent();
+	private goBack() {
+		let pageHistoryMock: Array<PageAliases> = this.state["pageHistory"];
+		pageHistoryMock.pop();
+		const lastPage = pageHistoryMock[pageHistoryMock.length - 1];
+		this.setState({ pageHistory: pageHistoryMock, currentPage: lastPage });
+	}
 
-        return component;
-    }
+	private buildComponent() {
+		let component = (
+			<SafeAreaView>
+				<StatusBar barStyle={"light-content"} />
+				<View style={Styles.screen}>{this.getPageToRender()}</View>
+			</SafeAreaView>
+		);
+
+		return component;
+	}
+
+	render() {
+		let component = this.buildComponent();
+
+		return component;
+	}
 }
 
 registerRootComponent(FolconnApp);
--- src/client/ClaraClient/src/view/components/LoginForm.tsx
+++ src/client/ClaraClient/src/view/components/LoginForm.tsx
@@ -5,75 +5,84 @@ import { FolconnButton } from "./FolconnButton";
 import { FolconnInput } from "./FolconnInput";
 
 interface LoginFormProps {
-    redirectPageFunction: Function;
+	redirectPageFunction: Function;
 }
 
 interface LoginFormState {
-    typedUserName: string,
-    typedPassword: string;
-
+	typedUserName: string;
+	typedPassword: string;
 }
 
 export class LoginForm extends Component<LoginFormProps, LoginFormState> {
-    constructor(props: LoginFormProps) {
-        super(props);
+	constructor(props: LoginFormProps) {
+		super(props);
 
-        this.state = {
-            typedUserName: "",
-            typedPassword: ""
-        };
+		this.state = {
+			typedUserName: "",
+			typedPassword: "",
+		};
 
-        this.submitLoginForm = this.submitLoginForm.bind(this);
-        this.receiveTypedPassword = this.receiveTypedPassword.bind(this);
-        this.receiveTypedUserName = this.receiveTypedUserName.bind(this);
+		this.submitLoginForm = this.submitLoginForm.bind(this);
+		this.receiveTypedPassword = this.receiveTypedPassword.bind(this);
+		this.receiveTypedUserName = this.receiveTypedUserName.bind(this);
+	}
 
-    }
+	private receiveTypedUserName(typedUserName: string) {
+		this.setState({ typedUserName: typedUserName });
+	}
 
-    private receiveTypedUserName(typedUserName: string) {
-        this.setState({ typedUserName: typedUserName });
+	private receiveTypedPassword(typedPassword: string) {
+		this.setState({ typedPassword: typedPassword });
+	}
 
-    }
+	private submitLoginForm() {
+		const userName = this.state["typedUserName"];
+		const password = this.state["typedPassword"];
 
-    private receiveTypedPassword(typedPassword: string) {
-        this.setState({ typedPassword: typedPassword });
+		if (userName === "" || password === "") {
+			//Alert.alert("Credenciais inválidas", "Os campos de usuário e senha não devem ser vazios");
+			Alert.alert(
+				"Invalid credentials",
+				"The username and password fields must be not empty"
+			);
+		} else {
+			Alert.alert("Realizando login");
 
-    }
+			this.props.redirectPageFunction("Home");
+		}
+	}
 
-    private submitLoginForm() {
-        const userName = this.state["typedUserName"];
-        const password = this.state["typedPassword"];
+	private buildComponent() {
+		let component = (
+			<View style={[Styles.formModal, Styles.shadow]}>
+				<Text style={Styles.title}>Welcome!</Text>
+				<View>
+					<FolconnInput
+						placeholder="Username"
+						type="username"
+						ejectFunction={this.receiveTypedUserName}
+						secureText={false}
+					/>
+					<FolconnInput
+						placeholder="Password"
+						type="password"
+						ejectFunction={this.receiveTypedPassword}
+						secureText={true}
+					/>
+					<FolconnButton
+						text="Log in"
+						onClick={this.submitLoginForm}
+					/>
+				</View>
+			</View>
+		);
 
-        if (userName === "" || password === "") {
-            //Alert.alert("Credenciais inválidas", "Os campos de usuário e senha não devem ser vazios");
-            Alert.alert("Invalid credentials", "The username and password fields must be not empty");
+		return component;
+	}
 
-        } else {
-            Alert.alert("Realizando login");
+	render() {
+		let component = this.buildComponent();
 
-            this.props.redirectPageFunction("Login")
-
-        }
-
-    }
-
-    private buildComponent() {
-        let component = (
-            <View style={[Styles.formModal, Styles.shadow]}>
-                <Text style={Styles.title}>Welcome!</Text>
-                <View>
-                    <FolconnInput placeholder="Username" type="username" ejectFunction={this.receiveTypedUserName} secureText={false} />
-                    <FolconnInput placeholder="Password" type="password" ejectFunction={this.receiveTypedPassword} secureText={true} />
-                    <FolconnButton text="Log in" onClick={this.submitLoginForm} />
-                </View>
-            </View>
-        );
-
-        return component;
-    }
-
-    render() {
-        let component = this.buildComponent();
-
-        return component;
-    }
+		return component;
+	}
 }
