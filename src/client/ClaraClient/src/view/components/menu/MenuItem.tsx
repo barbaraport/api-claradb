721388d63950bf4c9c929019df84d4993724b0dc
Bárbara Port <barbara.port@outlook.com>
2022-04-06 08:51:12 -0300

    refactoring menu item to a component

---
+++ src/client/ClaraClient/src/view/components/menu/MenuItem.tsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import { Colors } from "../../../enumerations/Colors";
+import { MaterialIcons } from '@expo/vector-icons';
+import { Sizes } from "../../../enumerations/Sizes";
+import { Styles } from "../../styles/Styles";
+
+interface MenuItemProps {
+     iconName: string,
+     itemName: string
+}
+
+interface MenuItemState { }
+
+export class MenuItem extends Component<MenuItemProps, MenuItemState> {
+
+     private buildMenuItemComponent() {
+          return (
+               <View style={Styles.menuItem}>
+                    <MaterialIcons name={this.props.iconName} size={Sizes.ICON} color={Colors.BLACK} />
+                    <Text style={Styles.text}>{this.props.itemName}</Text>
+               </View>
+          );
+     }
+
+     render() {
+          return (
+               let component = this.buildMenuItemComponent();
+
+               return component;
+          );
+     }
+}
