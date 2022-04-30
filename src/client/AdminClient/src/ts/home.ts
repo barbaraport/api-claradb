28826127711c677febd13da57128d193d57e1a58
Rafael Furtado <rafs.97@hotmail.com>
2022-04-30 16:08:57 -0300

    Criando menu

--- src/client/AdminClient/src/ts/home.ts
+++ src/client/AdminClient/src/ts/home.ts
@@ -1,1 +1,21 @@
-console.log("a");
+export function openMenu() {
+    const menuOptions = document.getElementById("menuOptions")!;
+
+    const isShow = menuOptions.classList.contains("show");
+
+    if(!isShow) {
+        menuOptions.classList.add("show");
+        
+    }
+}
+
+export function closeMenu() {
+    const menuOptions = document.getElementById("menuOptions")!;
+
+    const isShow = menuOptions.classList.contains("show");
+
+    if(isShow) {
+        menuOptions.classList.remove("show");
+        
+    }
+}
