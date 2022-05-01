cb77802385dc657136feceb58f36791bbaf6507b
Rafael Furtado <rafs.97@hotmail.com>
2022-05-01 19:08:42 -0300

    Exibindo resultados da pesquisa

--- src/client/AdminClient/src/ts/appAccess.ts
+++ src/client/AdminClient/src/ts/appAccess.ts
@@ -88,8 +88,67 @@ export function searchFols() {
 
         textElement.classList.add("folSearchResult");
 
+        textElement.onclick = function(this) {
+            const targetLabel = this as HTMLLabelElement;
+
+            const labelText = targetLabel.innerText;
+
+            const country = labelText.split(" - ")[0];
+
+            showCountryAccesses(country);
+
+        }
+
         searchResultOutput.appendChild(textElement);
 
     }
 
+}
+
+function showCountryAccesses(country: string) {
+    const accessesOutputElement = document.getElementById("searchResult")! as HTMLDivElement;
+
+    accessesOutputElement.innerHTML = "";
+
+    const stringfiedAttemptsData = sessionStorage.getItem(StorageKeys.LOGIN_ATTEMPTS_DATA);
+
+    if(stringfiedAttemptsData) {
+        const attemptsData = JSON.parse(stringfiedAttemptsData) as Array<LoginAttempt>;
+
+        for (let i = 0; i < attemptsData.length; i++) {
+            const loginAttempt = attemptsData[i];
+            
+            if(loginAttempt['country'] === country){
+                const resultContainer = document.createElement("tr");
+                resultContainer.classList.add("accessResult");
+
+                const userNameLabel = document.createElement("td");
+                userNameLabel.innerText = loginAttempt['userName'];
+
+                const userLocationLabel = document.createElement("td");
+                userLocationLabel.innerText = loginAttempt['city'] + ", " + loginAttempt['country'];
+
+                const userAccessDateLabel = document.createElement("td");
+                userAccessDateLabel.innerText = loginAttempt['date'];
+
+                resultContainer.appendChild(userNameLabel);
+                resultContainer.appendChild(userLocationLabel);
+                resultContainer.appendChild(userAccessDateLabel);
+
+                accessesOutputElement.appendChild(resultContainer);
+
+            }
+
+        }
+
+    }
+
+
+    /*
+    <div class="accessResult">
+        <label>User X</label>
+        <label>São José dos Campos, São Paulo</label>
+        <label>16/09/1997 10:00:00</label>
+    </div>
+    */
 }
