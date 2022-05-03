d5f0e6f471675d53dd658532ccfab0ddf617a1f0
Rafael Furtado <rafs.97@hotmail.com>
2022-05-03 07:32:26 -0300

    Corrigindo localização

--- src/server/src/models/services/locationService.py
+++ src/server/src/models/services/locationService.py
@@ -2,6 +2,7 @@ from geopy.geocoders import Nominatim
 
 geolocator = Nominatim(user_agent="claraDBFolconnApp")
 
+
 def getCoordinatePlace(position):
     latitude = position["lat"]
     longitude = position["long"]
@@ -10,13 +11,14 @@ def getCoordinatePlace(position):
 
     location = geolocator.reverse(stringToGetGeolocation).raw["address"]
 
+    print(location)
+
     usefulLocation = {
         "suburb": location["suburb"],
-        "city_district": location["city_district"],
         "town": location["town"],
         "state": location["state"],
         "country": location["country"],
-        "country_code": location["country_code"]
+        "city": location["city"]
     }
 
     return usefulLocation
