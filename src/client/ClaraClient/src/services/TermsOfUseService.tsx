9390f2661d0efd39abe8f55205440cf47ea400ea
Rafael Furtado <rafs.97@hotmail.com>
2022-05-04 08:36:55 -0300

    Permitindo ao usuário alterar aceite de termos de uso

--- src/client/ClaraClient/src/services/TermsOfUseService.tsx
+++ src/client/ClaraClient/src/services/TermsOfUseService.tsx
@@ -1,3 +1,4 @@
+import { Alert } from "react-native";
 import { ApiAccess } from "../enumerations/ApiAccess";
 
 export class TermsOfUseService {
@@ -19,5 +20,34 @@ export class TermsOfUseService {
           }
 
           throw new Error(`There was an error to get the Terms Of Use`);
+     }
+
+     public static async acceptTermsOfUse(accept: boolean, userId: string) {
+          let request = {
+               method: "POST",
+               body: JSON.stringify({accept: accept, userId: userId}),
+               headers: {
+                    "Content-Type": 'application/json',
+               }
+          }
+          
+          const response = await fetch("http://" + ApiAccess.host + ":" + ApiAccess.port + "/termsOfUse/accept", request);
+          
+          if (!response["ok"]) {
+               throw new Error(`Can not change the state of the acceptance of the terms of use`);
+          }
+          
+          let message = "The terms of use was ";
+
+          if(accept === true){
+               message += "accepted";
+
+          }else {
+               message += "declined";
+
+          }
+
+          Alert.alert(message);
+          
      }
 }
