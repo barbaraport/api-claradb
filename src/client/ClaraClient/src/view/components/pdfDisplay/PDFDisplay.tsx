830050574977b9a3d4fa7e43072e60ca3f62232f
Bárbara Port <barbara.port@outlook.com>
2022-05-13 20:17:42 -0300

    displaying message for not found FOLs

--- src/client/ClaraClient/src/view/components/pdfDisplay/PDFDisplay.tsx
+++ src/client/ClaraClient/src/view/components/pdfDisplay/PDFDisplay.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { Modal, TouchableOpacity, View } from "react-native";
+import { Modal, Text, TouchableOpacity, View } from "react-native";
 import Pdf from "react-native-pdf";
+import { Colors } from "../../../enumerations/Colors";
 import { FOLService } from "../../../services/FOLService";
 import { LocationService } from "../../../services/LocationService";
 import { Styles } from "../../assets/styles/Styles";
@@ -15,6 +16,7 @@ interface PDFDisplayState {
     fol: string;
     folFirstPage: number;
     showPdf: boolean;
+    message: string;
 }
 
 export class PDFDisplay extends Component<PDFDisplayProps, PDFDisplayState> {
@@ -28,7 +30,8 @@ export class PDFDisplay extends Component<PDFDisplayProps, PDFDisplayState> {
         this.state = {
             fol: "",
             folFirstPage: 0,
-            showPdf: true
+            showPdf: true,
+            message: "Please, wait..."
         }
 
         this.closePdfDisplay = this.closePdfDisplay.bind(this);
@@ -40,11 +43,14 @@ export class PDFDisplay extends Component<PDFDisplayProps, PDFDisplayState> {
 
         let folFirstPageNumber = await this.getFolFirstPage();
         this.setState({ folFirstPage: folFirstPageNumber });
-
+        
+        if (folBase64 === "data:application/pdf;base64," || folFirstPageNumber === 0) {
+            this.setState({ message: "Document not available :(" });
+        }
         this.registerLocation();
     }
 
-    private async registerLocation () {
+    private async registerLocation() {
         let position = await this.locationService.getUserPosition();
         await this.folService.registerFolAccess(this.props.folTitle, this.props.userID, position);
     }
@@ -64,14 +70,28 @@ export class PDFDisplay extends Component<PDFDisplayProps, PDFDisplayState> {
     private getPdfRenderer() {
         let view = <>
             <Modal transparent={true}>
-                <TouchableOpacity style={{ height: 120 }} activeOpacity={1} onPress={this.closePdfDisplay} />
-                <View style={{ flex: 1, justifyContent: "flex-start", alignItems: "center", height: "100%" }}>
-                    <Pdf
-                        page={this.state.folFirstPage}
-                        source={{ uri: this.state.fol }}
-                        style={Styles.pdf}
-                    />
-                </View>
+                {
+                    this.state.fol !== "" && this.state.folFirstPage !== 0 ?
+                        <>
+                            <TouchableOpacity style={{ height: 120 }} activeOpacity={1} onPress={this.closePdfDisplay} />
+                            <View style={{ flex: 1, justifyContent: "flex-start", alignItems: "center", height: "100%" }}>
+                                <Pdf
+                                    page={this.state.folFirstPage}
+                                    source={{ uri: this.state.fol }}
+                                    style={Styles.pdf}
+                                />
+                            </View>
+                        </>
+                        : <>
+                            <Modal transparent={true}>
+                                <TouchableOpacity style={{ height: "100%" }} activeOpacity={1} onPress={this.closePdfDisplay}>
+                                    <View style={{ alignSelf: "center", justifyContent: "center", alignItems: "center", marginTop: 120, height: "80%", width: "90%", backgroundColor: Colors.WHITE, borderRadius: 8, shadowColor: "#000", shadowOffset: { width: 0, height: 0, }, shadowOpacity: 0.50, shadowRadius: 4.22, elevation: 5 }}>
+                                        <Text style={{ fontSize: 14, fontWeight: "bold", marginTop: 20, marginBottom: 20, alignSelf: "center" }}>{this.state.message}</Text>
+                                    </View>
+                                </TouchableOpacity>
+                            </Modal>
+                        </>
+                }
             </Modal>
         </>
 
