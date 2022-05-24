4555e71986e397a268927698c0d825ec729b7452
Bárbara Port <barbara.port@outlook.com>
2022-05-24 10:46:29 -0300

    handling notifications in the background

--- src/client/ClaraClient/index.js
+++ src/client/ClaraClient/index.js
@@ -1,4 +1,10 @@
 import { AppRegistry } from 'react-native';
 import { FolconnApp } from './src/FolconnApp';
+import messaging from "@react-native-firebase/messaging";
+
+// Register background handler
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+     console.log('Message handled in the background!', remoteMessage);
+});
 
 AppRegistry.registerComponent("Folconn", () => FolconnApp);
